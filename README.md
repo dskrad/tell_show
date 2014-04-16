0bfceba9097e3ac73515dd2f45c2786d55404155
dskrad_pi <dskrad@me.com>
2014-04-15 21:55:02 -0400

    Initialize repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tell Show
+
+This is a simple script to remind me of when my favorite shows are on. 
+
+A single python script (and a single `.tell_show` file) are all that are needed.
+
+The script can be run manually, or you can automate the script with `crontab -e` on \*nix systems
+
+You can have the script send you results to your email or phone (Twitter, Pushbullet, etc), but I have not included these extra files because I did not want to share my personal API keys.
---
+++ tell_show.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import datetime as dt
+import re
+from time import sleep
+from urllib import urlopen
+from bs4 import BeautifulSoup as bs
+
+base = "http://www.thefutoncritic.com/listings/"
+now = dt.date.today()
+
+def check_shows(date,*shows):
+  url = base+date.strftime("%Y/%m/%d/")
+  soup = bs(urlopen(url))
+  msg = [date.strftime("%a %m/%d")]
+  for show in shows:
+    pat = re.compile(r"(?:\n)(%s)" % show)
+    regmatch = re.search(pat, soup.text)
+    if regmatch != None:
+      msg.append(regmatch.group(1).upper())
+  if len(msg) > 1:  
+    return " * ".join(msg) 
+
+def week_of_shows(*shows):
+  for i in range(7):
+    day = now + dt.timedelta(i)
+    yield check_shows(day,*shows)
+
+shws = [x.strip() for x in open(".tell_show","r").readlines()]
+for i in week_of_shows(*shws):
+  print i
