52d9c3f44855301f54615cfe534851b7a710ab46
dskrad_pi <dskrad@me.com>
2014-04-28 14:12:26 -0400

    Updates pushbullet.py to use requests

--- pushbullet.py
+++ pushbullet.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python
-from urllib import urlopen, urlencode
+import requests as r
 import sys
 import os
 key = os.environ['PUSHBULLET_APIKEY']
 device = os.environ['PUSHBULLET_IPHONE']
-url = "https://%s:@api.pushbullet.com/api/pushes" % key
+url = "https://api.pushbullet.com/api/pushes"
 
 def note(title, body):
-  params = urlencode({
+  params = {
     "device_iden": device,
     "type": "note",
     "title": title,
     "body": body
-    })
-  urlopen(url, params)
+  }
+  r.post(url, auth=(key,""), data=params)
 
 def link(title, url):
-  params = urlencode({
+  params = {
     "device_iden": device,
     "type": "link",
     "title": title,
     "url": url 
-    })
-  urlopen(url, params)
+  }
+  r.post(url, auth=(key,""), data=params)
 
 def main():
-  ntype, title, url_body = sys.argv[1:]
-  if ntype == "note":
-    note(title, url_body)
-  if ntype == "link":
-    link(title, url_body)
+  if len(sys.argv) != 4:
+    print "Incorrect number of args. Usage:"
+    print "\tpushbullet note note_title note_body"
+    print "\tpushbullet link title url"
+  else:
+    ntype, title, url_body = sys.argv[1:]
+    if ntype == "note":
+      note(title, url_body)
+    if ntype == "link":
+      link(title, url_body)
 
 if __name__ == "__main__":
   main()
